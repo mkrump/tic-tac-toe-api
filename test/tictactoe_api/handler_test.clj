26be0854e2e459223f0950e3b7183cc430a9d6b1
mkrump <mkrump@8thlight.com>
2017-06-07 11:30:49 -0500

    Add move validation

--- test/tictactoe_api/handler_test.clj
+++ test/tictactoe_api/handler_test.clj
@@ -1,14 +1,34 @@
 (ns tictactoe-api.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
+            [clojure.data.json :as json]
+            [tictactoe.board :as board]
             [tictactoe-api.handler :refer :all]))
 
-(deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
+(deftest valid-move-test
+  (testing "move valid returns 200 :valid true"
+    (let [test-request {:move 1, :board {:board-contents [1,0,0,0,0,0,0,0,0] :gridsize 3}}
+          expected {:board {:board-contents [1,0,0,0,0,0,0,0,0], :gridsize 3}, :valid true}
+          response (app (-> (mock/request :post "/valid-move")
+                            (mock/content-type "application/json")
+                            (mock/body (json/json-str test-request))))]
       (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (json/read-str (get-in response [:body]))) expected)
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8")))))
 
-  (testing "not-found route"
-    (let [response (app (mock/request :get "/invalid"))]
+(deftest square-occupied-test
+  (testing "square occupied returns 206 :valid false"
+    (let [test-request {:move 0, :board {:board-contents [1,0,0,0,0,0,0,0,0] :gridsize 3}}
+          expected {:board {:board-contents [1,0,0,0,0,0,0,0,0], :gridsize 3}, :valid false}
+          response (app (-> (mock/request :post "/valid-move")
+                            (mock/content-type "application/json")
+                            (mock/body (json/json-str test-request))))]
+      (is (= (:status response) 206))
+      (is (= (json/read-str (get-in response [:body]))) expected)
+      (is (= (get-in response [:headers "Content-Type"]) "application/json; charset=utf-8")))))
+
+(deftest invalid-route
+  (testing "invlaid-route"
+    (let [response (app (-> (mock/request :get "/no-route-here")))]
       (is (= (:status response) 404)))))
+
