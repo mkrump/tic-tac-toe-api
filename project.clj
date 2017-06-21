f459500e82ba3d02a67a3bd4aa816cdb663f7eec
mkrump <mkrump@8thlight.com>
2017-06-20 20:52:31 -0500

    Add travis + heroku

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject tictactoe-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure tictactoe api"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [compojure "1.5.1"]
